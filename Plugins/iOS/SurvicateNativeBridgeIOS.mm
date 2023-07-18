90b76ff2ea0cbffb3ff18a26341f29b35c2e1bde
jkalamarz-survicate <jakub.k@survicate.com>
2023-07-18 10:32:59 +0200

    Split iOS into setWorkspace and Init

--- Plugins/iOS/SurvicateNativeBridgeIOS.mm
+++ Plugins/iOS/SurvicateNativeBridgeIOS.mm
@@ -4,6 +4,8 @@
 extern "C" {
 void setWorkspaceKey(const char* appKey){
     [SurvicateSdk.shared setWorkspaceKey:[NSString stringWithUTF8String:appKey], error: nil];
-    [SurvicateSdk.shared initialize];
 }
+
+void initialize(){
+    [SurvicateSdk.shared initialize];
 }
