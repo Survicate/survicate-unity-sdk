32ab92c4ff7aec2677c1d50e13cbd8658b6f126a
SurvicatePawelGodlewski <112856109+SurvicatePawelGodlewski@users.noreply.github.com>
2024-05-07 07:40:48 +0200

    Add Survicate event listener support for iOS and Android (#4)
    * Support for SurvicateEventListener in iOS and Android

---
+++ Plugins/SurvicateEventListener.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plugins.Survicate
+{
+    public class SurvicateEventListener
+    {
+        private readonly Action<SurveyDisplayedEvent> _onSurveyDisplayed;
+        private readonly Action<QuestionAnsweredEvent> _onQuestionAnswered;
+        private readonly Action<SurveyClosedEvent> _onSurveyClosed;
+        private readonly Action<SurveyCompletedEvent> _onSurveyCompleted;
+
+        public SurvicateEventListener(
+            Action<SurveyDisplayedEvent> onSurveyDisplayed = null,
+            Action<QuestionAnsweredEvent> onQuestionAnswered = null,
+            Action<SurveyClosedEvent> onSurveyClosed = null,
+            Action<SurveyCompletedEvent> onSurveyCompleted = null
+        )
+        {
+            _onSurveyDisplayed = onSurveyDisplayed;
+            _onQuestionAnswered = onQuestionAnswered;
+            _onSurveyClosed = onSurveyClosed;
+            _onSurveyCompleted = onSurveyCompleted;
+        }
+
+        public void OnSurveyDisplayed(SurveyDisplayedEvent surveyDisplayedEvent)
+        {
+            _onSurveyDisplayed?.Invoke(surveyDisplayedEvent);
+        }
+
+        public void OnQuestionAnswered(QuestionAnsweredEvent questionAnsweredEvent)
+        {
+            _onQuestionAnswered?.Invoke(questionAnsweredEvent);
+        }
+
+        public void OnSurveyClosed(SurveyClosedEvent surveyClosedEvent)
+        {
+            _onSurveyClosed?.Invoke(surveyClosedEvent);
+        }
+
+        public void OnSurveyCompleted(SurveyCompletedEvent surveyCompletedEvent)
+        {
+            _onSurveyCompleted?.Invoke(surveyCompletedEvent);
+        }
+    }
+}
